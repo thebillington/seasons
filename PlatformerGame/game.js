8383754289df2b7a348040bc4fdb94627f11e09c
Dank5 <l.danks@newcastle.ac.uk>
2018-02-18 15:15:39 +0000

    Added function to change seasons as well as populating season objects for makeWater function

--- PlatformerGame/game.js
+++ PlatformerGame/game.js
@@ -13,6 +13,15 @@ var gridSquareSizeY;
 // Variable to hold all of our game objects
 var gameObjects;
 
+// Variable used to switch seasons (0 = summer, 1 = autumn, 2 = winter, 3 =  spring)
+var currentSeason;
+
+// Variables to store specific state of game objects for each season
+var summerObjects;
+var autumnObjects;
+var winterObjects;
+var springObjects;
+
 // Get the player sprite and rectangle
 var playerImg;
 var playerRect;
@@ -22,6 +31,7 @@ PLAYER_SPEED = 3;
 
 // Key codes
 SPACE = 32;
+KEY_S = 83;
 
 // Load objects in before the game loads
 function preload() {
@@ -43,19 +53,30 @@ function setup() {
     
     // Get the player rectangle
     playerRect = Rectangle(300, 50, gridSquareSizeX, 2*gridSquareSizeY);
+
+    // Initilise season object arrays
+    summerObjects = [];
+    autumnObjects = [];
+    winterObjects = [];
+    springObjects = [];
     
     // Create an empty array to hold the game objects
     gameObjects = [];
-  
+
+    // Initilise the season to summer
+    currentSeason = 0;
+    
     // ground creation:
-    makeGround(10, 20, 20, 1, "floor");
-    makeGround(20, 9, 20, 1, "floor");
+    makeGround(10, 20, 20, 1);
+    makeGround(20, 9, 20, 1);
     // tree creation
     makeTree(16,19,4,"evergreen");
     // water creation
-    makeWater(20, 19, 10, 2, "lake");
+    makeWater(20, 19, 10, 2);
     // terrarin creation
-    makeRock(10, 19, 2, "boulder");
+    makeRock(10, 19, 2);
+
+    gameObjects = summerObjects;
     
 }
 
@@ -84,8 +105,27 @@ function draw() {
     if (keyIsDown(LEFT_ARROW)) {
         playerRect.x-=PLAYER_SPEED;
     }
+
     
 }
+
+function changeSeason(currentSeason) {
+    switch(currentSeason) {
+        case(0):
+            gameObjects = summerObjects;
+            break;
+        case (1):
+            gameObjects = autumnObjects;
+            break;
+        case (2):
+            gameObjects = winterObjects;
+            break;
+        case (3):
+            gameObjects = springObjects;
+            break;
+    }
+}
+
 
 // Function to check key presses
 function keyPressed() {
@@ -100,5 +140,15 @@ function keyPressed() {
         jumped = true;
         
     }
-    
+
+    if (KEY_S) {
+        currentSeason++;
+        if(currentSeason > 3) {
+            currentSeason = 0;
+        }
+        changeSeason(currentSeason);
+        
+
+        console.log(currentSeason);
+    }
 }
--- PlatformerGame/objects.js
+++ PlatformerGame/objects.js
@@ -29,14 +29,29 @@ function makeTree(x, y, height, type) {
         }
         
         // Add the trunk game objects
-        gameObjects.push(GameObject(x, y - i, color(83, 53, 10), tC, "trunk"+type));
+        summerObjects.push(GameObject(x, y - i, color(83, 53, 10), tC, "trunk "+type));
+        // autumnObjects.push(GameObject(x, y - i, color(83, 53, 10), tC, "trunk "+type));
+        // winterObjects.push(GameObject(x, y - i, color(83, 53, 10), tC, "trunk "+type));
+        // if(height = i-1){
+        //     winterObjects.push(GameObject(x, y - i, color(255,255,255), true, "trunk "))
+        // }
+        // springObjects.push(GameObject(x, y - i, color(83, 53, 10), tC, "trunk "+type));
         
         // Make the leaves
         for (var j = i; j < height; j++) {
             
             // Add a leaf
-            gameObjects.push(GameObject(x+j-height+1,y-height-i+1, color(50,230,90), true, "leaf"+type));
-            gameObjects.push(GameObject(x-j+height-1,y-height-i+1, color(50,230,90), true, "leaf"+type));
+            summerObjects.push(GameObject(x+j-height+1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+            summerObjects.push(GameObject(x-j+height-1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+
+            // autumnObjects.push(GameObject(x+j-height+1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+            // autumnObjects.push(GameObject(x-j+height-1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+
+            // winterObjects.push(GameObject(x+j-height+1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+            // winterObjects.push(GameObject(x-j+height-1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+
+            // springObjects.push(GameObject(x+j-height+1,y-height-i+1, color(50,230,90), true, "leaf "+type));
+
             
         }
         
@@ -45,42 +60,53 @@ function makeTree(x, y, height, type) {
 }
 
 // Function to create water
-function makeWater(x, y, length, height, type) {
-        
+function makeWater(x, y, length, height) {
         //for the length of the water
         for (var i = 0; i < length; i++) {
             //and for the height of the water
             for(var j = 0; j < height; j++) {
                 //incrementally add gameObjects with appropriate colour and type
-                gameObjects.push(GameObject(x + i , y - j, color(104, 120, 201), "water"+type))
+                summerObjects.push(GameObject(x + i , y - j, color('rgba(104, 120, 201, 0)'), "drywater"))
+                autumnObjects.push(GameObject(x + i , y - j, color(104, 120, 201), "water"))
+                winterObjects.push(GameObject(x + i , y - j, color(165, 242, 243), "ice"))
+                springObjects.push(GameObject(x + i , y - j, color(104, 120, 201), "water"))
             }
+        }
 
-        }       
-
+        
 }
 
 // Function to create a ground
-function makeGround(x, y, length, height, type) {
+function makeGround(x, y, length, height) {
     
     //for the length of the ground
     for (var i = 0; i < length; i++) {
         //and for the height of the ground
         for(var j = 0; j < height; j++) {
             //incrementally add gameObjects with appropriate colour and type
-            gameObjects.push(GameObject(x + i , y - j, color(81, 67, 15), "ground"+type))
+            summerObjects.push(GameObject(x + i , y - j, color(81, 67, 15), "ground"))
+            // autumnObjects.push(GameObject(x + i , y - j, color(81, 67, 15), "ground"))
+            // winterObjects.push(GameObject(x + i , y - j, color(255, 0, 0), "ground"))
+            // springObjects.push(GameObject(x + i , y - j, color(81, 67, 15), "ground"))
         }
 
     }       
 
 }
 
-function makeRock(x, y, height, type) {
+function makeRock(x, y, height) {
 
         //for loops to iterate through height of the rock (Square object so height/length are interchangable)
         for (var i = 0; i < height; i++) {
             for(var j = 0; j < height; j++) {
                 //incrementally add gameObjects with appropriate colour and type
-                gameObjects.push(GameObject(x + i, y - j, color(90, 77, 65), "rock"+type))
+                summerObjects.push(GameObject(x + i, y - j, color(90, 77, 65), "rock"))
+                // autumnObjects.push(GameObject(x + i, y - j, color(90, 77, 65), "rock"))
+                // winterObjects.push(GameObject(x + i, y - j, color(90, 77, 65), "rock"))
+                // if(height = j-1){
+                //     winterObjects.push(GameObject(x + i, y - j, color(255, 0, 0), "rock"))
+                // }
+                // springObjects.push(GameObject(x + i, y - j, color(90, 77, 65), "rock"))
         }
     }
 }
