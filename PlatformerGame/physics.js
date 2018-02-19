9c022bef6cfd02fce5fe9ee138854593aed48bf0
Dank5 <l.danks@newcastle.ac.uk>
2018-02-19 14:13:00 +0000

    Make player collidable with rocks on x

--- PlatformerGame/physics.js
+++ PlatformerGame/physics.js
@@ -172,7 +172,7 @@ function playerCollision() {
         }
         
         // If the game object is ground or ice
-        if (gameObjects[i].type == "ground" || gameObjects.type == "ice") {
+        if ((gameObjects[i].type == "ground" || gameObjects[i].type == "ice") || gameObjects[i].type == "rock") {
             
             // Check for side collision
             checkSideCollision(playerRect, {x: gameObjects[i].x * gridSquareSizeX, y: gameObjects[i].y * gridSquareSizeY, width: gridSquareSizeX, height:gridSquareSizeY});
